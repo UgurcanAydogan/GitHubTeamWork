f3beccfe442dac37f8a51d67559a643e7975bf81
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-12-14 11:40:14 +0300

    first commit

---
+++ src/ortakclass.java
@@ -0,0 +1,5 @@
+public class ortakclass {
+    public static void main(String[] args) {
+        System.out.println("buradan bir ugurcan gecti...");
+    }
+}
---
+++ src/ugurcan.java
@@ -0,0 +1,18 @@
+public class ugurcan {
+    public static void main(String[] args) {
+        System.out.println("ugurca'nın yeri");
+
+    /*
+    git init
+    git add .
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/UgurcanAydogan/GitHubTeamWork.git
+    git push -u origin main
+     */
+
+
+
+    }
+
+}
